01530783f3a9481bc0d4e6d517da6aedf9a09db1
LalithaBhavani S <kasyaplalithaa@gmail.com>
2024-03-19 11:11:43 +0000

    hehe

--- fast.py
+++ fast.py
@@ -10,9 +10,10 @@ templates = Jinja2Templates(directory="templates")
 async def login_page(request: Request, username: str = None):
     return templates.TemplateResponse("login.html", {"request": request, "username": username})
 
-@app.post("/login")
-async def login(username: str = Form(...), password: str = Form(...)):
+@app.post("/submit_form")
+async def login(username: str = Form(...), firstName: str = Form(...), lastName: str = Form(...), officeName: str = Form(...), designation: str = Form(...)):
     # Here you can add your login logic
-    return {"username": username, "password": password}
+    return {"username": username, "firstName": firstName, "lastName": lastName, "officeName": officeName, "designation": designation}
 if __name__ == "__main__":
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
